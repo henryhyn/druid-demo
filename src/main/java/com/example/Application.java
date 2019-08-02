fe1021f4bbb115b402818dafcf9b7a3f68dc53cd
何永能 <yongnenghe@xiaohongshu.com>
2019-08-02 10:48:39 +0800

    select by page

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.dao.entity.WebsiteSearchSuggestKeyword;
 import com.example.dao.mapper.UserMapper;
 import com.example.dao.mapper.WebsiteSearchSuggestKeywordMapper;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.CollectionUtils;
 
 /**
  * Created by henry on 2019/8/1.
@@ -32,6 +35,18 @@ public class Application implements CommandLineRunner {
         QueryWrapper<WebsiteSearchSuggestKeyword> wrapper = new QueryWrapper<>();
         wrapper.lambda().eq(WebsiteSearchSuggestKeyword::getPkid, "xxx2");
         System.out.println(websiteSearchSuggestKeywordMapper.selectOne(wrapper));
+
+        System.out.println("======> Keyword Page:");
+        int pageIndex = 0;
+        Page<WebsiteSearchSuggestKeyword> page;
+        IPage<WebsiteSearchSuggestKeyword> iPage;
+        do {
+            pageIndex++;
+            page = new Page<>(pageIndex, 2);
+            System.out.println("===> pageIndex = " + pageIndex);
+            iPage = websiteSearchSuggestKeywordMapper.selectPage(page, null);
+            iPage.getRecords().forEach(System.out::println);
+        } while (!CollectionUtils.isEmpty(iPage.getRecords()));
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/web/config/MybatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.example.web.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by henry on 2019/8/2.
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
+    }
+}
